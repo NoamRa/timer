8996fc8fc3c929180d0e506cab2cf9356d5e0fb2
NoamRa <noamraby@gmail.com>
2019-05-10 16:51:47 +0300

    refactor time display, handle edge cases

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,28 @@ import "./App.css";
 import CirclePart from "./Components/CirclePart";
 import useInterval from "./logic/useInterval";
 import TimeInput from "./Components/TimeInput";
+import TimeDisplay from "./Components/TimeDisplay";
 
 const initialTimerDuration: number = 25 * 60; // in seconds
+const initialDegrees: number = 0.01 // will show full circle
 
 const App: React.FC = (): JSX.Element => {
   const getNow = (): number => Math.trunc(new Date().getTime() / 1000);
-  const calcDegreesPerSecond = (timerDuration: number) => 360 / timerDuration;
+  const calcDegreesPerSecond = (timerDuration: number) =>
+    // protect division by zero
+    timerDuration === 0 ? 360 : 360 / timerDuration;
 
   // init
   const now: number = getNow();
   // timerDuration is in seconds
-  let [timerDuration, setTimerDuration] = useState(initialTimerDuration);
-  let [startTime, setStartTime] = useState(now);
+  let [timerDuration, setTimerDuration] = useState<number>(
+    initialTimerDuration
+  );
+  let [startTime, setStartTime] = useState<number>(now);
   let degreesPerSecond: number = calcDegreesPerSecond(timerDuration);
-  let [degrees, setDegrees] = useState(degreesPerSecond * (startTime - now));
-  let [showNumericTimer, setShowNumericTimer] = useState(false);
+  let [degrees, setDegrees] = useState<number>(initialDegrees);
+  let [showNumericTimer, setShowNumericTimer] = useState<boolean>(false);
+  let [delay, setDelay] = useState<number | null>(1000);
 
   const handleTimeChange = (evt: SyntheticEvent): void => {
     const updatedTimerDuration: number =
@@ -27,27 +34,16 @@ const App: React.FC = (): JSX.Element => {
 
     const now = getNow();
     setStartTime(now);
-    degrees = degreesPerSecond * (now - startTime);
+    setDegrees(initialDegrees);
+    setDelay(1000);
   };
 
   useInterval(() => {
     // update remaining time every second
     const secondsPassed: number = getNow() - startTime;
     setDegrees(degreesPerSecond * secondsPassed);
-  }, 1000);
-
-  const renderTimeDisplay = (): JSX.Element => {
-    const now: number = getNow();
-    const passed: number = now - startTime;
-    const remaining = Math.abs(now - startTime - timerDuration);
-    return (
-      <React.Fragment>
-        Time Passed - {`${Math.trunc(passed / 60)}:${passed % 60}`}
-        <br />
-        Time Remaining - {`${Math.trunc(remaining / 60)}:${remaining % 60}`}
-      </React.Fragment>
-    );
-  };
+    secondsPassed >= timerDuration && setDelay(null);
+  }, delay!);
 
   return (
     <div className="app">
@@ -56,13 +52,20 @@ const App: React.FC = (): JSX.Element => {
         value={timerDuration / 60}
         onChange={handleTimeChange}
       />
-      <CirclePart diameter={500} degrees={degrees} />
-      <input
-        type="checkbox"
-        checked={showNumericTimer}
+
+      {delay ? (
+        <CirclePart diameter={400} degrees={degrees || initialDegrees} />
+      ) : (
+        <h1>your time is up!</h1>
+      )}
+
+      <TimeDisplay
+        now={now}
+        startTime={startTime}
+        timerDuration={timerDuration}
+        showNumericTimer={showNumericTimer}
         onChange={() => setShowNumericTimer(!showNumericTimer)}
       />
-      {showNumericTimer && renderTimeDisplay()}
     </div>
   );
 };
